f4d166a6548e14e56b337c3ea789203766714f25
timeoutme <383480701@qq.com>
2019-09-24 01:24:05 +0800

    更新注册页面和添加模板

--- ph/ph/settings.py
+++ ph/ph/settings.py
@@ -120,5 +120,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATICFILES = [
+    os.path.join(BASE_DIR, 'products/static/')
+]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
