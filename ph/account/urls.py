5da62c11b9688d8b663d0ca6a9f0562454469a6c
temp <383480701@qq.com>
2019-09-24 16:42:17 +0800

    添加登录页面和完善后台登录功能

--- ph/account/urls.py
+++ ph/account/urls.py
@@ -4,4 +4,5 @@ from .views import register_page, login
 urlpatterns = [
     path('signup/', register_page,name='注册页面'),
     path('login/',login,name='登录页面'),
+    
 ]
--- ph/account/views.py
+++ ph/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 def register_page(request):
@@ -17,14 +18,18 @@ def register_page(request):
                 User.objects.create_user(username=user_name,password=password1)
                 return redirect('主页')
             else:
-                return render(request,'register_page.html',{'密码错误':'两次输入的密码不一致'})          
+                return render(request,'register_page.html',{'密码错误':'两次输入的密码不一致'})       
     
-    
-
-
-
-
-
 
 def login(request):
-    return render(request,'login.html')    
+    if request.method == 'GET':
+        return render(request,'login.html')   
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        password1 = request.POST['密码']
+        user = auth.authenticate(username=user_name,password=password1)
+        if user == None:
+            return render(request,'login.html', {'错误':'用户名或密码错误'})
+        else:
+            auth.login(request,user)
+            return redirect('主页')         
