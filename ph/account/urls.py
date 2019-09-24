38a3b75d77a971884640ebc8b672c04a919804d5
temp <383480701@qq.com>
2019-09-24 16:06:03 +0800

    完善注册逻辑和后台功能

--- ph/account/urls.py
+++ ph/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import register_page
+from .views import register_page, login
 
 urlpatterns = [
-    path('', register_page,name='注册页面'),
+    path('signup/', register_page,name='注册页面'),
+    path('login/',login,name='登录页面'),
 ]
--- ph/account/views.py
+++ ph/account/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
 
 def register_page(request):
-    return render(request,'register_page.html')
+    if request.method == 'GET':
+        return render(request,'register_page.html')
+    elif request.method == 'POST':
+        user_name = request.POST['用户名']
+        password1 = request.POST['密码']
+        password2 = request.POST['确认密码']   
+        try:
+            User.objects.get(username=user_name)
+            return render(request,'register_page.html',{'用户名错误':'改用户名已存在'})
+        except User.DoesNotExist:
+            if password1 == password2:
+                User.objects.create_user(username=user_name,password=password1)
+                return redirect('主页')
+            else:
+                return render(request,'register_page.html',{'密码错误':'两次输入的密码不一致'})          
+    
+    
+
+
+
+
+
+
+def login(request):
+    return render(request,'login.html')    
